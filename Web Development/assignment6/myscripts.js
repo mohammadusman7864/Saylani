c846470063d650d63bb43fff18114e9fa3df4bcc
Muhammad <mohammadusman7864@gmail.com>
2021-12-02 17:04:01 +0500

    Ok Thanks

---
+++ Web Development/assignment6/myscripts.js
@@ -0,0 +1,35 @@
+
+/*
+question 1
+var a=prompt("first number");
+var b=prompt("second number");
+if(a>b && a!==b)  {
+    console.log("Maximum Number",a );
+}
+else{
+    if(b>a && a!==b)
+    console.log("Maximum Number",b );
+else{
+    console.log("Maximum does not Number" );
+}
+      }
+*/
+
+/*
+question2
+var a=prompt("first number");
+var b=prompt("second number");
+var c=prompt("second number");
+if(a>b && a>c){
+    console.log("Maximum Number",a );
+}
+else{
+    if(b>a && b>c)  
+    console.log("Maximum Number",b );
+    else{
+        console.log("Maximum Number",c );
+    }
+}
+*/
+
+
