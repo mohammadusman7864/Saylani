9bf6e193ce79887e276c3984aa1093c9a8e5a718
Muhammad Usman <mohammadusman7864@gmail.com>
2022-01-27 08:32:00 +0500

    Best of Luck

--- Web Development/New folder/assignment/src/App.js
+++ Web Development/New folder/assignment/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Header from "./components/header";
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import './html/Navbar/navbar.css'
+import Navbar from "./html/Navbar/Navbar";
+
 function App() {
   return (
     <div>
-    <Header/>
+    <Navbar/>
  
     </div>
   );
---
+++ Web Development/New folder/assignment/src/html/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+
+function Navbar() {
+    return (
+      <div className="container-fluid nav_bg">
+        <div className="row">
+          <div className="col-11">
+      <nav className="navbar navbar-expand-lg">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">bensik</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav justify-content-end">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About us</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
+
+      </ul>
+      <form className="d-flex">
+        <button className="btn btn-outline-success" type="submit">sign up</button>
+        <button className="btn btn-outline-success" type="submit">sign in</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Navbar;
+  
