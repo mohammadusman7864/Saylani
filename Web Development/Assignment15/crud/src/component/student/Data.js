bf81c04c3acaf8eb4a1f1cb821ce76fff8ac0c38
Muhammad Usman <mohammadusman7864@gmail.com>
2022-03-13 11:45:25 +0500

    of

--- Web Development/Assignment15/crud/src/component/student/Data.js
+++ Web Development/Assignment15/crud/src/component/student/Data.js
@@ -1,39 +1,39 @@
 export const Data = [{
-        name: "adnan",
+        name: "usman",
         batch: "4",
-        roll: "8098",
+        roll: "4633",
         class: "Web and mobile",
     },
     {
         name: "umar",
         batch: "4",
-        roll: "8098",
+        roll: "4633",
         class: "Web and mobile",
     },
 
     {
         name: "Ahmad",
         batch: "4",
-        roll: "8098",
+        roll: "4633",
         class: "Web and mobile",
     },
     {
         name: "hassan",
         batch: "4",
-        roll: "8098",
+        roll: "4633",
         class: "Web and mobile",
         id: Math.random * 875667,
     },
     {
         name: "ali",
         batch: "4",
-        roll: "8098",
+        roll: "4633",
         class: "Web and mobile",
     },
     {
         name: "asif",
         batch: "4",
-        roll: "8098",
+        roll: "4633",
         class: "Web and mobile",
     }
 ]
