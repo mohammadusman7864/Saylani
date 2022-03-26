c7f548cf77276475e8f5721fd9688f5c07f6db3d
Muhammad Usman <mohammadusman7864@gmail.com>
2022-03-26 15:18:37 +0500

    thank

--- Web Development/Assignment16/assignment/src/App.js
+++ Web Development/Assignment16/assignment/src/App.js
@@ -1,15 +1,22 @@
-
+import { Grid } from "@material-ui/core";
 import React from 'react'
 import Create from './component/Create/Create'
 import Navbar from './component/Navbar/Navbar'
-
+import Leftbar from './component/Leftbar/Leftbar'
 
 function App() {
   return (
     <div>
-      <Navbar/>
-     <Create/>
-      </div>
+      <Navbar />
+      <Grid container>
+        <Grid item sm={2} xs={2}>
+          <Leftbar />
+        </Grid>
+        <Grid item sm={7} xs={10}>
+          <Create />
+        </Grid>
+      </Grid>
+    </div>
   )
 }
 
